d9db93fe771f22ef20d64409f82fc10cc5ca657c
petervanderhook <petervanderhook@gmail.com>
2023-12-11 00:15:13 -0800

    lol

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "bucket_name" {
-  default = "pv-4630-ass3-bigbucket3"
+  default = "pv-4640-ass3-bigbucket3"
 }
 variable "dynamodb_name" {
-  default = "pv-4630-ass3-stronglock3"
+  default = "pv-4640-ass3-stronglock3"
 }
 variable "region" {
   default = "us-west-1"

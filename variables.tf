15bf10a2ee79b913955ee94aa7d3413c9db05ef1
petervanderhook <petervanderhook@gmail.com>
2023-12-15 09:23:34 -0800

    Removed some files

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  default = "pv-4640-ass3-bigbucket3"
-}
-variable "dynamodb_name" {
-  default = "pv-4640-ass3-stronglock3"
-}
-variable "region" {
-  default = "us-west-1"
-}
